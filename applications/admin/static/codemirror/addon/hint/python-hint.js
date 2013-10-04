4712c62246c6d852fce65673188e21fc06407a9d
qdoop <github@qdoop.com>
2013-10-04 07:54:42 +0300

    _qdoop_

--- applications/admin/static/codemirror/addon/hint/python-hint.js
+++ applications/admin/static/codemirror/addon/hint/python-hint.js
@@ -20,8 +20,9 @@
     // Find the token at the cursor
     var cur = editor.getCursor(), token = getToken(editor, cur), tprop = token;
     // If it's not a 'word-style' token, ignore the token.
-
-	console.log('\r\nstring="'+token.string+'"| type='+token.type+'| state='+token.state.dedent+','+token.state.lastToken);
+	
+    //_qdoop_
+    console.log('\r\nstring="'+token.string+'"| type='+token.type+'| state='+token.state.dedent+','+token.state.lastToken);
 	
     if (!/^[\w$_]*$/.test(token.string)) {
         token = tprop = {start: cur.ch, end: cur.ch, string: "", state: token.state,
@@ -90,7 +91,7 @@
         base = base[context.pop().string];
       if (base != null) gatherCompletions(base);
     }
-	
+	//_qdoop_
 	found.push('zzzzx0');
 	found.push('zzzzx1');
 	
